5895c278b91f9b35a3e0908309bdcf904a1e5338
minha <alsgk6989@gmail.com>
2023-07-18 14:39:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# COOLDOWN
+* bigdata.cl6oki5w3xec.ap-northeast-2.rds.amazonaws.com
---
+++ src/main/java/team/project/cooldown/CooldownApplication.java
@@ -0,0 +1,13 @@
+package team.project.cooldown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooldownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooldownApplication.class, args);
+    }
+
+}
