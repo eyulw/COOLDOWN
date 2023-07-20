9b9277944ffe1e4c689b7ef6746f4266fd289dae
minha <alsgk6989@gmail.com>
2023-07-20 16:00:17 +0900

    admin : UserService - readUser구현

---
+++ src/main/java/team/project/cooldown/service/admin/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package team.project.cooldown.service.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import team.project.cooldown.dao.admin.UserDAO;
+import team.project.cooldown.model.User;
+
+import java.util.List;
+
+@Service("a_usrv")
+public class UserServiceImpl implements UserService{
+
+    @Autowired private UserDAO a_udao;
+    @Override
+    public List<User> readUser() {
+        return a_udao.selectUser();
+    }
+}
