5242884bb8e9138dbb0c706d502e423475580a4c
minha <alsgk6989@gmail.com>
2023-07-20 16:00:03 +0900

    admin : UserMapper - selectUser구현

--- src/main/java/team/project/cooldown/mybatis/UserMapper.java
+++ src/main/java/team/project/cooldown/mybatis/UserMapper.java
@@ -1,8 +1,11 @@
 package team.project.cooldown.mybatis;
 
 import org.apache.ibatis.annotations.Mapper;
+import team.project.cooldown.model.User;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
-
+    List<User> selectUser();
 }
