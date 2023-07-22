5b60edb42cb08745da3cc74ab4a16e041ea395f7
wjdwo <wjdwo1104@gmail.com>
2023-07-22 21:36:27 +0900

    Cooldown.sql 테이블 작성

---
+++ src/main/java/team/project/cooldown/model/ItemAttach.java
@@ -0,0 +1,16 @@
+package team.project.cooldown.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ItemAttach {
+    private String iano;
+    private String item_id;
+    private String fname;
+    private String fsize;
+
+}
