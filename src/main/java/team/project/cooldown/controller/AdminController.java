b9b0256afac707d7fa35224481c7bbe4acb1373e
minha <alsgk6989@gmail.com>
2023-07-22 20:56:37 +0900

    admin관련 html,css,controller

--- src/main/java/team/project/cooldown/controller/AdminController.java
+++ src/main/java/team/project/cooldown/controller/AdminController.java
@@ -6,7 +6,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import team.project.cooldown.service.admin.QnaService;
 import team.project.cooldown.service.admin.UserService;
 
 @Controller
@@ -15,17 +17,18 @@ import team.project.cooldown.service.admin.UserService;
 public class AdminController {
     Logger logger = LogManager.getLogger(AdminController.class);
     final UserService a_usrv;
+    final QnaService a_qsrv;
 
     @GetMapping("/")
     public String index(){
         logger.info("admin/ 호출");
         return "admin/index";
     }
-    @GetMapping("/member")
+    @GetMapping("/user")
     public String member(Model m){
-        logger.info("admin/member 호출");
-        m.addAttribute("members",a_usrv.readUser());
-        return "admin/member";
+        logger.info("admin/user 호출");
+        m.addAttribute("users",a_usrv.readUser());
+        return "admin/user";
     }
 
     @GetMapping("/board")
@@ -34,4 +37,22 @@ public class AdminController {
         return "admin/board";
     }
 
+    @GetMapping("/item")
+    public String item(){
+        logger.info("admin/item 호출");
+        return "admin/item";
+    }
+
+    @GetMapping("/qna")
+    public String qna(Model m){
+        logger.info("admin/qna 호출");
+        m.addAttribute("qnas",a_qsrv.readQna());
+        return "admin/qna";
+    }
+
+    @GetMapping("/itemqna")
+    public String itemqna(){
+        logger.info("admin/itemqna 호출");
+        return "admin/itemqna";
+    }
 }
