5df703cf5265fcc77d9a2096ff239360ab9fcda0
wjdwo <wjdwo1104@gmail.com>
2023-07-22 21:37:12 +0900

    Cooldown.sql 테이블 작성

--- src/main/java/team/project/cooldown/mybatis/ItemMapper.java
+++ src/main/java/team/project/cooldown/mybatis/ItemMapper.java
@@ -1,8 +1,18 @@
 package team.project.cooldown.mybatis;
 
 import org.apache.ibatis.annotations.Mapper;
+import team.project.cooldown.model.ItemCombine;
+
+import java.util.List;
 
 @Mapper
 public interface ItemMapper {
+    List<ItemCombine> selectItemCombine();
+    List<ItemCombine> selectItemCombine_d();
+    List<ItemCombine> selectItemCombine_c();
+    List<ItemCombine> selectItemCombine_h();
+    List<ItemCombine> selectItemCombine_b();
+    List<ItemCombine> selectItemCombine_r();
+
 
 }
