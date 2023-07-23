d47be4ab3221a9bf8014440531d53040137db8b0
pinkmins2 <pinkmins2@gmail.com>
2023-07-23 21:12:32 +0900

    Board: BoardMapper - list, view, write 구현

--- src/main/java/team/project/cooldown/mybatis/BoardMapper.java
+++ src/main/java/team/project/cooldown/mybatis/BoardMapper.java
@@ -3,10 +3,20 @@ package team.project.cooldown.mybatis;
 import org.apache.ibatis.annotations.Mapper;
 import team.project.cooldown.model.Board;
 
+import java.util.List;
+
 @Mapper
 public interface BoardMapper {
 
     int insertBoard(Board b);
 
     int lastBoardBno();
+
+    int updateViewBoard(String board_id);
+
+    Board selectOneBoard(String board_id);
+
+    List<Board> selectBoard(int stnum);
+
+    int selectCountBoard();
 }
