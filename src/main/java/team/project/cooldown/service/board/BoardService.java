20161c0939f16a5595016b0e5477c60d5f9b0378
pinkmins2 <pinkmins2@gmail.com>
2023-07-23 21:13:30 +0900

    Board: BoardService - list, view, write 구현

--- src/main/java/team/project/cooldown/service/board/BoardService.java
+++ src/main/java/team/project/cooldown/service/board/BoardService.java
@@ -2,7 +2,15 @@ package team.project.cooldown.service.board;
 
 import team.project.cooldown.model.Board;
 
+import java.util.List;
+
 public interface BoardService {
 
-    int newBoard(Board b);
+    boolean saveBoard(Board b);
+
+    Board readOneBoard(String board_id);
+
+    List<Board> readBoard(Integer cpg);
+
+    int countBoard();
 }
