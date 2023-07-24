00b818ec10ea28f8ba630aa145592da6e4c9193b
pinkmins2 <pinkmins2@gmail.com>
2023-07-24 18:04:01 +0900

    Board: list, view, write 버튼 및 페이지네이션 구현

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -5,13 +5,30 @@ newbdbtn?.addEventListener('click', () => {
 });
 
 
+// board view
+let bdlist = document.querySelector("#bdlist")
+bdlist?.addEventListener('click', ()=> {
+    location.href = '/board/list/1';
+});
+
+
 // board write
+let bdwrtcancelbtn = document.querySelector("#bdwrtcancelbtn")
+bdwrtcancelbtn.addEventListener('click', ()=> {
+    location.href = '/board/list/1'
+})
 let wrtbdbtn = document.querySelector("#wrtbdbtn")
+
 wrtbdbtn?.addEventListener('click', () => {
     let frm = document.forms.post_form;
     if(frm.title.value === '') alert('제목을 작성하세요!!');
     else if(frm.contents.value === '') alert('본문을 작성하세요!!');
     else {
+        let b_category=document.querySelector("#b_category")
+        let categoryValue=document.querySelector("#categoryValue")
+        categoryValue.value=b_category.value;
+
+        frm.action='/board/write'
         frm.method = 'post';
         frm.submit();
     }
