31b918da17b9e8761e8c849098defe2439917c1e
toppingbox-climbing <just.come.as.you.are.babe@gmail.com>
2023-07-24 18:00:35 +0900

    회원가입: MemberJoinController 상위 RequestMapping 추가

--- src/main/java/team/project/cooldown/controller/MemberJoinController.java
+++ src/main/java/team/project/cooldown/controller/MemberJoinController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/join")
+
 public class MemberJoinController {
 
     Logger logger = LogManager.getLogger(MemberJoinController.class);
