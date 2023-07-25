6697461a0333c486c1f56b2680c1fa9493b16822
minha <alsgk6989@gmail.com>
2023-07-25 15:58:18 +0900

    admin - item 관련 css, js, controller

--- src/main/java/team/project/cooldown/controller/AdminController.java
+++ src/main/java/team/project/cooldown/controller/AdminController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import team.project.cooldown.model.Item;
 import team.project.cooldown.service.admin.QnaService;
 import team.project.cooldown.service.admin.A_UserService;
+import team.project.cooldown.service.board.BoardService;
+import team.project.cooldown.service.item.ItemService;
 
 @Controller
 @RequestMapping("/admin")
@@ -19,6 +23,8 @@ public class AdminController {
     Logger logger = LogManager.getLogger(AdminController.class);
     final A_UserService a_usrv;
     final QnaService a_qsrv;
+    final BoardService bsrv;
+    final ItemService isrv;
 
     @GetMapping("/")
     public String index(){
@@ -33,8 +39,9 @@ public class AdminController {
     }
 
     @GetMapping("/board")
-    public String board(){
+    public String board(Model m){
         logger.info("admin/board 호출");
+        m.addAttribute("bds",bsrv.readBoard(1));
         return "admin/board";
     }
 
@@ -42,6 +49,25 @@ public class AdminController {
     public String item(){
         logger.info("admin/item 호출");
         return "admin/item";
+    }
+
+    @GetMapping("/item/make")
+    public String makeitem(){
+        logger.info("admin/item/makeitem 호출");
+        return "admin/makeItem";
+    }
+
+    @PostMapping("/item/make")
+    public String makeitemok(Item i, MultipartFile attach){
+        logger.info("admin/item/makeitemok 호출");
+        String returnPage="redirect:/admin/fail";
+        int itemId=isrv.newItem(i);
+        if(!attach.isEmpty()){
+            isrv.newItemAttach(attach,itemId);
+            returnPage="redirect:/admin/item";
+        }
+
+        return returnPage;
     }
 
     @GetMapping("/qna")
