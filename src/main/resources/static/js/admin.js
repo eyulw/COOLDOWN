c90ea7dbde488ac7aacfd733927cdecc9afac0ad
minha <alsgk6989@gmail.com>
2023-07-27 17:51:34 +0900

    admin - html, css, js 수정,추가

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -52,8 +52,8 @@ makeItemBtn?.addEventListener('click',()=>{
 
 
 let mkitembtn=document.querySelector("#mkitembtn");
-let icategory=document.querySelector("#icategory");
 mkitembtn?.addEventListener('click',()=>{
+    let icategory=document.querySelector("#icategory");
     let frm=document.forms.mkitemfrm;
     if(frm.attach.value==='') alert("상품 이미지를 등록하세요");
     else if(frm.itemname.value==='') alert("상품명을 입력하세요");
@@ -70,8 +70,63 @@ mkitembtn?.addEventListener('click',()=>{
 })
 
 /*상품페이지 - 상품삭제*/
-let aitemDelete=document.querySelector("#aitemDelete");
-aitemDelete?.addEventListener('click',()=>{
-    let itemId=document.querySelector("#itemId");
-    location.href='/admin/itemDelete/'+itemId.value;
+$('.aitemDeleteBtn')?.on('click',(e)=>{
+    if(confirm("상품을 삭제하시겠어요?")){
+        location.href='/admin/itemDelete/'+e.target.value;
+    }
 })
+
+/*게시글페이지 - 게시글삭제*/
+$('.bdDelBtn')?.on('click',(e)=>{
+    if(confirm("게시글을 삭제하시겠어요?")){
+        location.href='/admin/boardDelete/'+e.target.value;
+    }
+})
+
+/*상품수정페이지*/
+$('.aitemModifyBtn')?.on('click',(e)=>{
+    if(confirm("상품을 수정하시겠어요?")){
+        location.href='/admin/item/modify/'+e.target.value;
+    }
+})
+
+let mditembtn=document.querySelector("#mditembtn");
+let cancelmdiBtn=document.querySelector("#cancelmdiBtn");
+cancelmdiBtn?.addEventListener('click',()=>{
+    location.href='/admin/item';
+})
+mditembtn?.addEventListener('click',()=>{
+    let icategory=document.querySelector("#icategory");
+    let frm=document.forms.mditemfrm;
+    if(frm.itemname.value==='') alert("상품명을 입력하세요");
+    else if(frm.price.value==='') alert("상품 가격을 입력하세요");
+    else if(frm.i_category.value==='-------선택-------') alert("상품 카테고리를 선택하세요");
+    else if(frm.info.value==='') alert("상품 상세정보를 입력하세요");
+    else if(frm.stock.value==='') alert("상품 재고를 입력하세요");
+    else{
+        icategory.value=frm.i_category.value;
+        if(frm.attach.value===''){
+            frm.action='/admin/item/modify/'+frm.oneiid.value;
+            frm.method='post';
+            frm.submit();
+        }else{
+            frm.action='/admin/item/modifyI/'+frm.oneiid.value;
+            frm.method='post';
+            frm.enctype='multipart/form-data';
+            frm.submit();
+        }
+    }
+})
+
+let irplok = document.querySelector("#irplok");
+irplok?.addEventListener('click',()=>{
+    let frm=document.forms.irplfrm;
+    let itemqnaid=document.querySelector("#itemqnaid");
+    if(frm.answer.value===''){
+        alert("문의에 대한 답변을 작성해 주세요");
+    } else{
+        frm.action="/admin/itemqna/"+itemqnaid.value;
+        frm.method='post';
+        frm.submit();
+    }
+});
