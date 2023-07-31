b968d751ca099a3372675d723baae11fa0ba42d9
toppingbox-climbing <just.come.as.you.are.babe@gmail.com>
2023-07-31 09:33:24 +0900

    마이페이지 : 줄 변경

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -19,20 +19,17 @@
 // }
 
 
-
 let wrtbdbtn = document.querySelector("#wrtbdbtn");
-    wrtbdbtn?.addEventListener('click', () => {
-        let frm = document.forms.qnafrm;
+wrtbdbtn?.addEventListener('click', () => {
+    let frm = document.forms.qnafrm;
 
-        if(frm.title.value ==='') alert('제목을 작성하세요!!');
-            else if(frm.contents.value === '') alert('본문을 작성하세요');
-
-            else {
-                frm.method ='post';
-                frm.submit();
-        }
-    });
-
+    if(frm.title.value ==='') alert('제목을 작성하세요!!');
+    else if(frm.contents.value === '') alert('본문을 작성하세요');
+    else {
+        frm.method ='post';
+        frm.submit();
+    }
+});
 
 // let listpopover = document.querySelector('button#listpopover');
 //
